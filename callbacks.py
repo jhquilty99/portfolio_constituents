3c22da59e5da1a625a226e2f574f8e64cd896181
jhquilty99 <jhquilty99@gmail.com>
2023-09-24 15:41:53 -0400

    Improved UI loading states

--- callbacks.py
+++ callbacks.py
@@ -1,31 +1,35 @@
 from dash.dependencies import Input, Output, State, ALL, MATCH
 from src.server.callback_functions import validate_stock
 from src.analysis.data_pull import get_current_prices, get_constituent_weights, get_prop_value, scrape_security_data
-from src.server.layout import app
-import plotly.express as px
+from src.server.layout import app, OUTPUT_LAYOUT_INITIAL, output_layout_final
 import pandas as pd
-import dash_ag_grid as dag
 
 @app.callback(
     Output('ticker-table', 'data'),
+    Output('ticker-validation','children'),
     Input('add-row', 'n_clicks'),
     State('enter-row-name', 'value'),
     State('enter-row-amount', 'value'),
     State('ticker-table', 'data'))
 def update_rows(n_clicks, name, amount, existing_rows):
-    if n_clicks > 0 and validate_stock(name):
-        for i, row in enumerate(existing_rows):
-            if row['ticker'] == name:
-                existing_rows[i] = {'ticker': name, 'amount': row['amount'] + int(amount)}
-                return existing_rows
-        existing_rows.append({'ticker': name, 'amount': int(amount)})
-    return existing_rows
+    if n_clicks > 0: 
+        if validate_stock(name):
+            for i, row in enumerate(existing_rows):
+                if row['ticker'] == name:
+                    existing_rows[i] = {'ticker': name, 'amount': row['amount'] + int(amount)}
+                    return (existing_rows, f'Added {amount} shares to {name}.')
+            existing_rows.append({'ticker': name, 'amount': int(amount)})
+            return (existing_rows, f'Added new ticker {name}, with {amount} shares.')
+        else: 
+            return (existing_rows, f'Could not find data for {name}.')
+    return (existing_rows, None)
 
-@app.callback(Output('constituents-table', 'children'),
-              Output('pie-graph', 'figure'),
+@app.callback(#Output('constituents-table', 'children'),
+              #Output('pie-graph', 'figure'),
+              Output('output-layout', 'children'),
               Input('save-portfolio','n_clicks'),
               State('ticker-table', 'data'),
-              prevent_initial_call = True)
+              prevent_initial_call = False)
 def display_output(n_clicks, rows):
     if n_clicks > 0:
         portfolio = pd.DataFrame(rows)
@@ -33,18 +37,16 @@ def display_output(n_clicks, rows):
         formatted_portfolio = portfolio.set_index('ticker')['amount'].to_dict()
         prop = get_prop_value(formatted_portfolio, prices)
         consts = scrape_security_data(list(portfolio['ticker']))
-        df = get_constituent_weights(consts, prop).reset_index()
-        df.columns = ['name', 'value']
-        df['value'] = df['value'].apply("{:,.4f}%".format)
-        constituents_table = dag.AgGrid(
-            rowData=df.to_dict("records"),
-            columnDefs=[{"field": i, "sortable": True} for i in df.columns],
-        )
+        consts_df = get_constituent_weights(consts, prop).reset_index()
+        consts_df.columns = ['name', 'value']
+        #df['value'] = df['value']*100
+        #df['value'] = df['value'].apply("{:,.2f}%".format)
         portfolio['result'] = portfolio['ticker'].map(prices) * portfolio['amount']
         portfolio['result'] = portfolio['result'].apply(round, ndigits = 2)
         portfolio['result_formatted'] = portfolio['result'].apply("${:,.2f}".format)
-        output_graph = px.pie(portfolio, names='ticker', values='result', hover_data=['result_formatted'], hole=0.3)
-    return (constituents_table, output_graph)
+        return output_layout_final(consts_df, portfolio)
+    else:
+        return OUTPUT_LAYOUT_INITIAL
 
 # Run the app
 if __name__ == '__main__':
--- src/server/layout.py
+++ src/server/layout.py
@@ -2,6 +2,8 @@
 from dash import dcc, html, Dash, dash_table
 import dash_bootstrap_components as dbc
 from flask import Flask
+import dash_ag_grid as dag
+import plotly.express as px
 
 #Instantiates the Dash app and identify the server
 server = Flask(__name__)
@@ -26,7 +28,60 @@ OUTER_STYLE = {
     'width':'100%'
 }
 
+INTRO_TEXT = '''
+    This website is intended to pull the aggregate 
+    holdings of all your etfs together into one view. 
+    You can add new ETFs or increase the holdings of current ETFs in the portfolio by
+    using the forms to enter the ticker and shares, and then click 'Submit.' 
+    You can also remove ETFs by clicking the 'X' button next to their name.
+    When you're done setting the portfolio, click 'Save Portfolio' to visualize the 
+    distribution of underlying holdings in your ETFs.
+'''
+
 DEFAULT_TICKERS = ['VGT','EDV','QQQ']
+
+
+OUTPUT_LAYOUT_INITIAL = [
+    html.P('Visualize ETF distribution here, after clicking "Save Portfolio"', 
+           style={
+                'padding': '10px', 
+                'max-width': '720px', 
+                'margin': '10px auto',
+                'width':'100%'
+            })
+]
+
+def output_layout_final(constituents_df, portfolio_df):
+    return([
+        # Second Section
+        html.Div([
+            # Title
+            html.H1("Portfolio Constituents"),
+            # Constituents Section
+            # dcc.Dropdown(disabled=True),
+            # Table Section
+            html.Br(),
+            dag.AgGrid(
+                rowData=constituents_df.to_dict("records"),
+                columnDefs=[
+                    {"field": 'name', "sortable": True, "unSortIcon": True},
+                    {"field": 'value', "sortable": True, "sort":"desc", 
+                    "unSortIcon": True, "valueFormatter": {"function": "d3.format(',.1%')(params.value)"}}],
+                columnSize="sizeToFit"
+        ),
+        ], style=TOP_STYLE),
+
+        # Third Section
+        html.Div([
+            # Title
+            html.H1("Portfolio Value Distribution"),
+            # Pie Graph Section
+            dcc.Graph(figure = px.pie(
+                portfolio_df, names='ticker', values='result', 
+                hover_data=['result_formatted'], hole=0.3
+            )),
+        ], style=TOP_STYLE)
+    ])
 
 # Define the app layout with outer container for centering
 app.layout = html.Div([
@@ -34,23 +89,29 @@ app.layout = html.Div([
     html.Div([
         # Title
         html.H1("Portfolio"),
+        html.P(INTRO_TEXT, style={"fontSize": "12px"}),
         # Add to Portfolio Section
         html.Div([
             dcc.Input(
                 id='enter-row-name',
-                placeholder='Enter an ETF name here...',
+                placeholder='ETF name here...',
                 type='text',
-                style={'padding': 10,'width':'25%'}
+                style={'padding': 10,'width':'30%'}
             ),
             dcc.Input(
                 id='enter-row-amount',
-                placeholder='Enter the number of shares here...',
+                placeholder='# of shares here...',
                 type='number',
                 min=1,
-                style={'padding': 10,'width':'49%'}
+                style={'padding': 10,'width':'40%'}
             ),
-            html.Button('Add Row', id='add-row', n_clicks=0, style={'padding': 10,'width':'19%'})
-        ], style={'height': 50}),
+            dbc.Button("Submit", id='add-row', n_clicks=0, 
+                       className="d-md-flex", style={'padding': 10,'width':'20%'})
+        ], style={'height': 50, 'display': 'flex', 
+                  'alignItems': 'center', 'justifyContent': 'space-between'}),
+        html.Br(),
+        dbc.Spinner(html.Div(id='ticker-validation')),
+        html.Br(),
         # Portfolio Table Section
         dash_table.DataTable(
             id='ticker-table',
@@ -62,28 +123,13 @@ app.layout = html.Div([
                 {'ticker': i, 'amount': 1} for i in DEFAULT_TICKERS
             ],
             row_deletable=True,
-            cell_selectable=False
+            cell_selectable=False,
+            style_data={'alignItems': 'center'}
         ),
         # Save Button
         html.Br(),
-        html.Button('Save Portfolio', id='save-portfolio', n_clicks=0),
+        dbc.Button('Save Portfolio', id='save-portfolio', 
+                   n_clicks=0, className="w-100"),
     ], style=TOP_STYLE),
-
-    # Second Section
-    html.Div([
-        # Title
-        html.H1("Portfolio Constituents"),
-        # Constituents Section
-        # dcc.Dropdown(disabled=True),
-        # Table Section
-        html.Div(id = 'constituents-table')
-    ], style=TOP_STYLE),
-
-    # Third Section
-    html.Div([
-        # Title
-        html.H1("Portfolio Value Distribution"),
-        # Pie Graph Section
-        dcc.Graph(id="pie-graph"),
-    ], style=TOP_STYLE),
+    html.Div(id = 'output-layout', style={'width':'100%'})
 ], style=OUTER_STYLE)
